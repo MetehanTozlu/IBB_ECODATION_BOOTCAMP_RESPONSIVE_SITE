0289c2f4d4a42ddf6e7918b5debab9ba50bc103c
Metehan Tozlu <metehan24x@gmail.com>
2023-07-06 20:49:19 +0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// Dark Mode 
+
+
+let darkModu = () => {
+    //alert("Dark modu");
+    document.body.classList.toggle("dark_mode");
+}
+
+
+
+// Footer
+// let newDate = () => {
+// const date = new Date().getFullYear();{
+
+// }
+     // JS DOM
+     //document.getElementById("date_id").innerHTML=new Date().getFullYear()
